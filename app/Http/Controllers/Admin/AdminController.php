2b800da7f5d1ccbc43b07fb6a40b29e5aa7ab396
Kirill Buziuk <45974315+last2828@users.noreply.github.com>
2020-04-27 10:12:59 +0300

    update

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use function Composer\Autoload\includeFile;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public static function index()
     {
-        return view('Admin.dashboard');
+//        return view('vendor/unisharp/laravel-filemanager/src/views/demo');
+        return PartialView('../vendor/unisharp/laravel-filemanager/src/views/demo');
     }
 }
