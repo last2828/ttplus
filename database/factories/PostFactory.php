f6a4db230800221419a7cf87f5366b38ddea7b59
Kirill Buziuk <last2828@gmail.com>
2021-01-28 12:56:01 +0200

    upload

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Post;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+
+$factory->define(Post::class, function (Faker $faker) {
+    $title = $faker->unique()->sentence(rand(3, 8), true);
+
+    $data = [
+        'title' => $title,
+        'content' => $faker->realText(),
+        'meta_title' => $faker->sentence(rand(3, 8), true),
+        'meta_description' => $faker->sentence(rand(3, 8), true),
+        'meta_keywords' => $faker->sentence(rand(3, 8), true),
+        'category_id' => rand(1, 3),
+        'type_id' => rand(1, 3),
+        'slug' => Str::slug($title),
+        'status' => $faker->boolean(50),
+    ];
+
+    return $data;
+});
