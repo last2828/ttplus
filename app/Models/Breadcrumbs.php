1d74bdbe556dd7748fccfd8280b9b29934b3e9f6
Kirill Buziuk <last2828@gmail.com>
2021-01-31 13:43:38 +0200

    Upload 1. Create method for Repositories and use it in ProductController

--- app/Models/Breadcrumbs.php
+++ app/Models/Breadcrumbs.php
@@ -4,32 +4,49 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Breadcrumbs
+ *
+ * @package App\Models
+ *
+ * Breadcrumbs for custom rendering catalogs for dunkernmotoren and jianhai
+ */
 class Breadcrumbs extends Model
 {
-  public static function renderBreadcrumbs($route, $dunker, $jianghai, $categories)
-  {
-    switch (true):
-      case ($route == 'pages.catalog.dunkermotoren.index'):
-        return \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('catalog-brand', $dunker);
-        break;
-      case ($route == 'pages.catalog.dunkermotoren.category'):
-        return \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('catalog-brand-category', $dunker, $categories->first());
-        break;
-      case ($route == 'pages.catalog.dunkermotoren.group'):
-        return \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('catalog-brand-category-group', $dunker, $categories->first(), $categories->first()->groups->first());
-        break;
+    /**
+     * @param $route
+     * @param $dunker
+     * @param $jianghai
+     * @param $categories
+     * @return \Illuminate\Support\HtmlString
+     * @throws \DaveJamesMiller\Breadcrumbs\Exceptions\InvalidBreadcrumbException
+     * @throws \DaveJamesMiller\Breadcrumbs\Exceptions\UnnamedRouteException
+     * @throws \DaveJamesMiller\Breadcrumbs\Exceptions\ViewNotSetException
+     */
+    public static function renderBreadcrumbs($route, $dunker, $jianghai, $categories)
+    {
+        switch (true):
+            case ($route == 'pages.catalog.dunkermotoren.index'):
+                return \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('catalog-brand', $dunker);
+                break;
+            case ($route == 'pages.catalog.dunkermotoren.category'):
+                return \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('catalog-brand-category', $dunker, $categories->first());
+                break;
+            case ($route == 'pages.catalog.dunkermotoren.group'):
+                return \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('catalog-brand-category-group', $dunker, $categories->first(), $categories->first()->groups->first());
+                break;
 
-    endswitch;
+        endswitch;
 
-    switch (true):
-      case ($route == 'pages.catalog.jianghai.index'):
-        return \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('catalog-brand', $jianghai);
-        break;
-      case ($route == 'pages.catalog.jianghai.category'):
-        return \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('catalog-brand-category', $jianghai, $categories->first());
-        break;
+        switch (true):
+            case ($route == 'pages.catalog.jianghai.index'):
+                return \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('catalog-brand', $jianghai);
+                break;
+            case ($route == 'pages.catalog.jianghai.category'):
+                return \DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::render('catalog-brand-category', $jianghai, $categories->first());
+                break;
 
-    endswitch;
-  }
+        endswitch;
+    }
 
 }
